eba80d58ca513a7b1b99a9b047927c1c47ed4557
m4dd0c <manishsuthar078@gmail.com>
2026-02-04 01:01:59 +0530

    feat : helloworld

---
+++ HelloWorld/SayHelloWorld.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Another way to do the same 
+// ns name should be same as filename
+namespace SayHelloWorld;
+class PrintHello 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World, Manish");
+    }
+}
